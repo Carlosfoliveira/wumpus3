bd2dc00779fccc946cc0d852021a3830c5c89841
Carlos Fernando de Oliveira Junior <carlos.fe.oliveira@gmail.com>
2013-11-18 19:33:48 -0300

    teste do dynamic

--- wumpus3.pl
+++ wumpus3.pl
@@ -1,8 +1,12 @@
 :- load_files([wumpus1]).
 
-init_agent :- 
-    writeln('Agente iniciando...'). 
+:-dynamic([davolta/1]).
+davolta([turnleft,turnleft,goforward]).
 
+init_agent :- 
+    writeln('Agente iniciando...'), 
+	retractall(davolta(_)),
+	assert(davolta([turnleft,goforward])).
 
 restart_agent :-
     init_agent.
@@ -16,9 +20,28 @@ run_agent(Pe,Ac):-
 	
 agente003([_,_,yes,_,_],grab). %pega o ouro%
 agente003([_,_,_,yes,_],turnleft). %quando bate vira a esquerda%
-agente003([yes,_,_,_,_],shoot). %quando sentir fedor atira%
-agente003([_,_,no,_,_],goforward). %anda para frente sempre%
-%agente003([no,no,_,_,_],goforward). quando nao sente brisa ou fedor anda para frente%
+%agente003([yes,_,_,_,no],shoot). %quando sentir fedor atira%
+agente003([no,no,no,_,_],goforward). %anda para frente sempre%
+%agente003([yes,_,_,_,yes],goforward). %qnd matar o wumpus anda para frente
+%agente003([no,no,_,_,_],goforward).% quando nao sente brisa ou fedor anda para frente%
+agente003([_,yes,_,_,_],P):-
+	davolta([P|T]),
+	retractall(davolta(_)),
+	assert(davolta(T)).
 
 
+
+
+
+
+
+
+% [Fedor,Vento,Brilho,Trombada,Grito]
+% Acoes possiveis:
+% goforward - andar
+% turnright - girar sentido horario
+% turnleft - girar sentido anti-horario
+% grab - pegar o ouro
+% climb - sair da caverna
+% shoot - atirar a flecha
                                                            
