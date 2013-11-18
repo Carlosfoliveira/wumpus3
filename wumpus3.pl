f1226ffcfd8d30a90c7376b5765e0d2c4d874a8b
Carlos Fernando de Oliveira Junior <carlos.fe.oliveira@gmail.com>
2013-11-18 12:12:10 -0300

    acrescentou comentario e atira flecha

--- wumpus3.pl
+++ wumpus3.pl
@@ -14,11 +14,11 @@ run_agent(Pe,Ac):-
 
 
 	
-agente003([_,_,yes,_,_],grab).
-agente003([_,_,_,yes,_],turnleft).
-
-agente003([_,_,no,_,_],goforward).
-%agente003([no,no,_,_,_],goforward).%
+agente003([_,_,yes,_,_],grab). %pega o ouro%
+agente003([_,_,_,yes,_],turnleft). %quando bate vira a esquerda%
+agente003([yes,_,_,_,_],shoot). %quando sentir fedor atira%
+agente003([_,_,no,_,_],goforward). %anda para frente sempre%
+%agente003([no,no,_,_,_],goforward). quando n sente brisa ou fedor anda para frente%
 
 
                                                            
