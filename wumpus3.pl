a6bfdc37e9f644cbf5445a6f58e5ab34770812f5
Carlos Fernando de Oliveira Junior <carlos.fe.oliveira@gmail.com>
2013-11-21 15:28:18 -0300

    Agente 1.0

--- wumpus3.pl
+++ wumpus3.pl
@@ -1,12 +1,15 @@
 :- load_files([wumpus1]).
 
-:-dynamic([davolta/1]).
-davolta([turnleft,turnleft,goforward]).
+:-dynamic([sair/1,atira/1,viradas/1]).
 
 init_agent :- 
     writeln('Agente iniciando...'), 
-	retractall(davolta(_)),
-	assert(davolta([turnleft,goforward])).
+	retractall(sair(_)),
+	retractall(atira(_)),
+	retractall(viradas(_)),
+	assert(sair([climb])),
+	assert(atira([shoot,goforward,goforward,goforward])),
+	assert(viradas(0)).
 
 restart_agent :-
     init_agent.
@@ -14,29 +17,43 @@ restart_agent :-
 
 run_agent(Pe,Ac):-
     agente003(Pe,Ac).
-
-
-
 	
 agente003([_,_,yes,_,_],grab). %pega o ouro%
-agente003([_,_,_,yes,_],turnleft). %quando bate vira a esquerda%
-%agente003([yes,_,_,_,no],shoot). %quando sentir fedor atira%
-agente003([no,no,no,_,_],goforward). %anda para frente sempre%
-%agente003([yes,_,_,_,yes],goforward). %qnd matar o wumpus anda para frente
-%agente003([no,no,_,_,_],goforward).% quando nao sente brisa ou fedor anda para frente%
+
 agente003([_,yes,_,_,_],P):-
-	davolta([P|T]),
-	retractall(davolta(_)),
-	assert(davolta(T)).
+    sair([P|T]),
+    retractall(sair(_)),
+    assert(sair(T)).
+%   assert(sair([turnleft,goforward])).
 
+agente003([_,_,_,yes,_],turnleft):- %quando bate a 1 vez vira a esquerda%
+	viradas(0),
+	somar(0).
 
+agente003([_,_,_,yes,_],turnleft):- %quando bate a 2 vez vira a esquerda%
+    viradas(1),
+    somar(1).
 
+agente003([_,_,_,yes,_],turnleft):- %quando bate a 3 vez vira a esquerda%
+    viradas(2),
+    somar(2).
 
+agente003([_,_,_,yes,_],climb):- %quando bate a 4 vez sai do mapa%
+    viradas(3).
 
+somar(X):-
+	retractall(viradas(_)),
+	Y is X+1,
+	assert(viradas(Y)).	
 
+agente003([yes,_,_,_,no],P):- %quando sentir fedor atira%
+	atira([P|T]),
+	retractall(atira(_)),
+	assert(atira(T)).
 
+agente003([_,_,no,_,no],goforward). %anda para frente%
 
-% [Fedor,Vento,Brilho,Trombada,Grito]
+%Fedor,Vento,Brilho,Trombada,Grito]
 % Acoes possiveis:
 % goforward - andar
 % turnright - girar sentido horario
