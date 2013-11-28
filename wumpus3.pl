406ee877cdb766a50cd88292d366773f187e7e4c
Carlos Fernando de Oliveira Junior <carlos.fe.oliveira@gmail.com>
2013-11-28 19:05:57 -0300

    Wumpus 2.0

--- wumpus3.pl
+++ wumpus3.pl
@@ -4,20 +4,20 @@
 
 init_agent :- 
     writeln('Agente iniciando...'), 
-	retractall(sair(_)),
+	retractall(sair(_)),%limpando todas as variaveis dinamicas
 	retractall(atira(_)),
 	retractall(viradas(_)),
 	retractall(gold(_)),
 	retractall(pegar(_)),
 	retractall(viraatira(_)),
 	retractall(casa(_)),
-	assert(sair([climb])),
-	assert(atira([shoot,goforward,goforward,goforward])),
-	assert(viradas(0)),
-	assert(gold(0)),
-	assert(pegar([grab,turnleft,turnleft])),
-	assert(viraatira([turnleft,shoot,goforward,goforward,goforward])),
-	assert(casa(0)).
+	assert(sair([climb])), %Quando sentir a 1 brisa vai dar climb, servindo para quando sentir brisa na casa (1,1) nao arriscar e sair logo
+	assert(atira([shoot,goforward,goforward,goforward])), %Quando sentir o Fedor sem ser na quinas do mapa vai atirar e andar ate sair do Fedor
+	assert(viradas(0)), %Conta o numero de vezes o Agente virou para sair quando bater a 4 vez ou para dar Backtracking
+	assert(gold(0)), %Saber se ja pegou o ouro para comecar o Backtracking
+	assert(pegar([grab,turnleft,turnleft])), %Quando pegar o ouro dar uma volta de 180 graus
+	assert(viraatira([turnleft,shoot,goforward,goforward,goforward])), %Quando estiver na quina do mapa e sentir uma brisa ele vira antes de atirar
+	assert(casa(0)). %Contar em que casa esta para saber se esta na quina ou nao
 
 restart_agent :-
     init_agent.
@@ -26,123 +26,118 @@ restart_agent :-
 run_agent(Pe,Ac):-
     agente003(Pe,Ac).
 	
-agente003([_,_,yes,_,_],grab):- %pega o ouro%
+agente003([_,_,yes,_,_],grab):- %pega o ouro e soma 1 ao ouro
 	somarouro(0).
 
-agente003([_,_,_,_,_],turnleft):-
+agente003([_,_,_,_,_],turnleft):- %Começando a dar o giro de 180 graus depois de pegar o ouro
 	gold(1),
 	somarouro(1).
 
-agente003([_,_,_,_,_],turnleft):-
+agente003([_,_,_,_,_],turnleft):- %Termino de giro de 180 grays depois de pegar o ouro
     gold(2),
     somarouro(2).
 
-agente003([_,yes,_,_,_],P):- %se houver brisa na casa (1,1) ele sai
-    sair([P|T]),
+agente003([_,yes,_,_,_],P):- %se houver brisa na casa (1,1) ele da climb
+	casa(0),
+    sair([P|T]), %pegando apenas a cabeça da variavel dinamica
     retractall(sair(_)),
-    assert(sair(T)).
+    assert(sair(T)). %adicionando a variavel dinamica menos a cabeça
 
-agente003([_,_,_,yes,_],turnleft):- %quando bate a 1 vez vira a esquerda%
+agente003([_,_,_,yes,_],turnleft):- %quando bate a 1 vez sem ouro vira a esquerda e zera as casas
 	gold(0),
 	viradas(0),
 	somarviradas(0),
 	zerarcasa(3).   
 
-agente003([_,_,_,yes,_],turnleft):- %quando bate a 2 vez vira a esquerda%
+agente003([_,_,_,yes,_],turnleft):- %quando bate a 2 vez sem ouro vira a esquerda e zera as casas
     gold(0),
 	viradas(1),
     somarviradas(1),
 	zerarcasa(3).
 	
 
-agente003([_,_,_,yes,_],turnleft):- %quando bate a 3 vez vira a esquerda%
+agente003([_,_,_,yes,_],turnleft):- %quando bate a 3 vez sem ouro vira a esquerda e zera as casas
     gold(0),
 	viradas(2),
     somarviradas(2),
 	zerarcasa(3).
 
-agente003([_,_,_,yes,_],climb):- %quando bate a 4 vez sai do mapa%
+agente003([_,_,_,yes,_],climb):- %quando bate a 4 vez sem ouro sai do mapa
     gold(0),
 	viradas(3).
 
-agente003([_,_,_,yes,_],turnright):- %quando bate a 3 vez vira a esquerda%
+agente003([_,_,_,yes,_],turnright):- %Começo do Backtracking, vai diminuindo as viradas ao longo que bate e virando a direita
     gold(3),
     viradas(3),
     diminuirviradas(3).
 
-agente003([_,_,_,yes,_],turnright):- %quando bate a 3 vez vira a esquerda%
+agente003([_,_,_,yes,_],turnright):- %quando virada 2 com ouro vira a direita
     gold(3),
     viradas(2),
     diminuirviradas(2).
 
-agente003([_,_,_,yes,_],turnright):- %quando bate a 3 vez vira a esquerda%
+agente003([_,_,_,yes,_],turnright):- %quando virada 1 com ouro vira a direita
     gold(3),
     viradas(1),
     diminuirviradas(1).
 
-agente003([_,_,_,yes,_],climb):- %quando bate a 3 vez vira a esquerda%
-    gold(3),
+agente003([_,_,_,yes,_],climb):- %quando virada chega a 0 com o ouro ele da climb para sair
+	gold(3),
     viradas(0).
 
 
 
-agente003([yes,_,_,_,no],P):- %quando sentir fedor atira%
+agente003([yes,_,_,_,no],P):- %quando sentir fedor na casa 0 atira para frente e continua andando
 	casa(0),
 	atira([P|T]),
 	retractall(atira(_)),
 	assert(atira(T)).
 
-agente003([yes,_,_,_,no],P):- %quando sentir fedor atira%
+agente003([yes,_,_,_,no],P):- %quando sentir fedor na casa 1 atira para frente e continua andando
 	casa(1),
     atira([P|T]),
     retractall(atira(_)),
     assert(atira(T)).
 
-agente003([yes,_,_,_,no],P):- %quando sentir fedor atira%
+agente003([yes,_,_,_,no],P):- %quando sentir fedor na casa 2 atira para frente e continua andando
 	casa(2),
     atira([P|T]),
     retractall(atira(_)),
     assert(atira(T)).
 
-agente003([yes,_,_,_,no],P):- %quando sentir fedor atira%
+agente003([yes,_,_,_,no],P):- %quando sentir fedor na casa 3, ou seja, em uma quina ele vira para esquerda e depois atira
 	casa(3),
     viraatira([P|T]),
     retractall(viraatira(_)),
     assert(viraatira(T)).
 
-agente003([_,_,no,_,no],goforward):- %anda para frente%
+agente003([_,_,no,_,no],goforward):- %anda para frente quando n sente brilho ou grito e vai somando 1 a casa toda vez q andar
 	casa(X),
 	somarcasa(X).
 
-somarviradas(X):-                      %funcao para somar o numero de viradas%
+somarviradas(X):-  %funcao para somar o numero de viradas
     retractall(viradas(_)),
     Y is X+1,
     assert(viradas(Y)).
 
-somarouro(X):-                      %funcao para somar o numero de ouro%
+somarouro(X):-  %funcao para somar o numero de ouro
     retractall(gold(_)),
     Y is X+1,
     assert(gold(Y)).
 
-diminuirviradas(X):-                      %funcao para somar o numero de viradas%
+diminuirviradas(X):-  %funcao para diminuir o numero de viradas
     retractall(viradas(_)),
     Y is X-1,
     assert(viradas(Y)).
 
-somarcasa(X):-
+somarcasa(X):- %funcao para somar o numero da casa
 	retractall(casa(_)),
 	Y is X+1,
 	assert(casa(Y)).
 
-zerarcasa(X):-
+zerarcasa(X):- %funcao para zerar o numero da casa
 	retractall(casa(_)),
 	Y is X-3,
 	assert(casa(Y)).
 
-
-direita(turnright).
-esquerda(turnleft).
-climb(climb).
-
-%Fedor,Vento,Brilho,Trombada,Grito]
-                                      
+                        
