1be190440d551510384c65dba8c43bce59eca8e9
Carlos Fernando de Oliveira Junior <carlos.fe.oliveira@gmail.com>
2013-11-29 18:39:06 -0300

    Wumpus 2.2

--- wumpus3.pl
+++ wumpus3.pl
@@ -16,7 +16,7 @@ init_agent :-
 	assert(viradas(0)), %Conta o numero de vezes o Agente virou para sair quando bater a 4 vez ou para dar Backtracking
 	assert(gold(0)), %Saber se ja pegou o ouro para comecar o Backtracking
 	assert(pegar([grab,turnleft,turnleft])), %Quando pegar o ouro dar uma volta de 180 graus
-	assert(viraatira([goforward,turnleft,shoot,goforward,goforward,goforward])), %Quando estiver na quina do mapa e sentir uma brisa ele vira antes de atirar
+	assert(viraatira([turnleft,shoot,goforward,goforward,goforward])), %Quando estiver na quina do mapa e sentir uma brisa ele vira antes de atirar
 	assert(casa(0)). %Contar em que casa esta para saber se esta na quina ou nao
 
 restart_agent :-
@@ -92,25 +92,28 @@ agente003([yes,_,_,_,no],P):- %quando sentir fedor na casa 0 atira para frente e
 	casa(0),
 	atira([P|T]),
 	retractall(atira(_)),
-	assert(atira(T)).
+	assert(atira(T)),
+	retractall(viraatira(_)).
 
 agente003([yes,_,_,_,no],P):- %quando sentir fedor na casa 1 atira para frente e continua andando
 	casa(1),
     atira([P|T]),
     retractall(atira(_)),
-    assert(atira(T)).
+    assert(atira(T)),
+	retractall(viraatira(_)).
 
 agente003([yes,_,_,_,no],P):- %quando sentir fedor na casa 2 atira para frente e continua andando
 	casa(2),
     atira([P|T]),
     retractall(atira(_)),
-    assert(atira(T)).
+    assert(atira(T)),
+	retractall(viraatira(_)).
 
-agente003([yes,_,_,_,no],P):- %quando sentir fedor na casa 3, ou seja, em uma quina ele vira para esquerda e depois atira
+agente003([yes,_,_,_,no],turnleft):- %quando sentir fedor na casa 3, ou seja, em uma quina ele vira para esquerda e depois atira
 	casa(3),
-	viraatira([P|T]),
-    retractall(viraatira(_)),
-    assert(viraatira(T)).
+	viradas(X),
+	somarviradas(X),
+	zerarcasa(3).
 
 agente003([_,_,no,_,no],goforward):- %anda para frente quando n sente brilho ou grito e vai somando 1 a casa toda vez q andar
 	casa(X),
